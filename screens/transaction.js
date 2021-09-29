12c5923c86ec12aba8daf3ff0e3f68c58b9edd17
delishadalu <73183936+delishadalu@users.noreply.github.com>
2021-09-29 22:25:46 +0530

    Add files via upload

---
+++ screens/transaction.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Transaction extends React.Component {
+    render() {
+        return (
+            <View style={{ backgroundColor:'teal',flex:1}}>
+                <Text style={{marginTop:200}}> issue or return</Text>
+
+            </View>
+        );
+    }
+}
