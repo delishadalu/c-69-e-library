8d3334757ac89c221a42d1d2f01f236c9f4590fa
delishadalu <73183936+delishadalu@users.noreply.github.com>
2021-09-29 22:33:11 +0530

    Update search.js

--- screens/search.js
+++ screens/search.js
@@ -6,7 +6,7 @@ export default class Search extends React.Component {
     render() {
         return (
             <View style={{ flex:1,backgroundColor: 'teal'}}>
-                <Text style={{marginTop:200}}>serchscreen</Text>
+                <Text style={{marginTop:200,alignSelf:"center"}}>serchscreen</Text>
 
             </View>
         );
